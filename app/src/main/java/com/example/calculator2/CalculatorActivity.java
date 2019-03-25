2489f64f1d10fbff2b7a1670f73c41db59c85a64
Puten <afaputen@list.ru>
2019-03-26 00:24:48 +0300

    calculator works

--- app/src/main/java/com/example/calculator2/CalculatorActivity.java
+++ app/src/main/java/com/example/calculator2/CalculatorActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -22,11 +23,22 @@ public class CalculatorActivity extends AppCompatActivity {
     Button but8;
     Button but9;
     Button but0;
+    Button butC;
+
+    Button butPlus;
+    Button butMinus;
+    Button butDiv;
+    Button butMult;
+    Button butEq;
+
     Button butPoint;
 
     Double num = 0.0;
+    Double numM = 0.0;
     Boolean pointMode = false;
     Integer digitsAfterPoint = 1;
+
+    String currentAction = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +61,13 @@ public class CalculatorActivity extends AppCompatActivity {
         but0 = findViewById(R.id.but0);
         butPoint = findViewById(R.id.but_point);
 
+        butC = findViewById(R.id.but_c);
+        butPlus = findViewById(R.id.but_plus);
+        butMinus = findViewById(R.id.but_minus);
+        butDiv = findViewById(R.id.but_divide);
+        butMult = findViewById(R.id.but_mult);
+        butEq = findViewById(R.id.but_eq);
+
         but1.setOnClickListener(view -> numClick(1));
         but2.setOnClickListener(view -> numClick(2));
         but3.setOnClickListener(view -> numClick(3));
@@ -60,6 +79,45 @@ public class CalculatorActivity extends AppCompatActivity {
         but9.setOnClickListener(view -> numClick(9));
         but0.setOnClickListener(view -> numClick(0));
         butPoint.setOnClickListener(view -> pointMode = true);
+
+        butC.setOnClickListener(view -> {
+            num = 0.0;
+            text.setText(num.toString());
+        });
+
+        butPlus.setOnClickListener(view -> actionClick("plus"));
+        butMinus.setOnClickListener(view -> actionClick("minus"));
+        butDiv.setOnClickListener(view -> actionClick("div"));
+        butMult.setOnClickListener(view -> actionClick("mult"));
+        butEq.setOnClickListener(view -> actionClick("eq"));
+    }
+
+    private void actionClick(String action) {
+        if ("plus".equals(currentAction)) {
+            num = numM + num;
+        }
+        if ("minus".equals(currentAction)) {
+            num = numM - num;
+        }
+        if ("mult".equals(currentAction)) {
+            num = numM * num;
+        }
+        if ("div".equals(currentAction)) {
+            if (num != 0) {
+                num = numM / num;
+            } else {
+                Toast.makeText(CalculatorActivity.this, "на ноль не делят", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (!("eq".equals(action))) {
+            numM = num;
+            num = 0.0;
+        }
+
+        currentAction = action;
+
+        text.setText(num.toString());
     }
 
     private void numClick(int value) {
@@ -70,7 +128,6 @@ public class CalculatorActivity extends AppCompatActivity {
         }
 
         text.setText(num.toString());
-
     }
 
     @Override
